cee83dbdfb71a4a1bf09e13660df7bdb70e0a809
Guy Carmeli <guyc@wix.com>
2023-07-04 21:06:33 +0300

    Another day, another test.

--- tests/unit/components/square/model/squareViewModel.test.ts
+++ tests/unit/components/square/model/squareViewModel.test.ts
@@ -1,19 +1,22 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useSquareViewModel } from "../../../../../src/components/square/model/squareViewModel";
 import { GameModel } from "../../../../../src/core/model/GameModel";
 import { mockDeep } from "jest-mock-extended";
 import { when } from "jest-when";
 import { spyOnGetter } from "../../../../utils/spyOnGetter";
+import { Observable } from "react-obsidian";
 
 describe('squareViewModel', () => {
   const index = 1;
+  let squares: Observable<string[]>;
   let model: GameModel;
 
   beforeEach(() => {
     model = mockDeep<GameModel>();
-    when(spyOnGetter(model.squares, 'value'))
+    squares = new Observable(['A', 'B', 'C']);
+    when(spyOnGetter(model, 'squares'))
       .calledWith()
-      .mockReturnValue(['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']);
+      .mockReturnValue(squares);
   });
 
   const renderUut = () => {
@@ -24,5 +27,18 @@ describe('squareViewModel', () => {
     const { result } = renderUut();
     result.current.onClick();
     expect(model.onSquareClick).toHaveBeenCalledWith(index);
+  });
+
+  it('should return the text from the model', () => {
+    const { result } = renderUut();
+    expect(result.current.text).toEqual('B');
+  });
+
+  it('should re-render when the model changes', () => {
+    const { result } = renderUut();
+    act(() => {
+      squares.value = ['D', 'E', 'F'];
+    });
+    expect(result.current.text).toEqual('E');
   });
 });
