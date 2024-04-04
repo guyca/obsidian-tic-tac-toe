62fa43888b2440d49246fb6f4fd475a937d4298c
Guy Carmeli <>
2024-04-04 22:18:41 +0300

    Initial commit to show coverage badge

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@
 const config = {
   testEnvironment: 'jsdom',
   coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
+  coverageReporters: ["json-summary", "text"],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!dist/**',
+  ],
+  collectCoverage: true,
 };
 
 module.exports = config;
